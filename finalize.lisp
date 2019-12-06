d0772549c1aaf49e22300da7decc7d708eaad377
Spenser Truex <web@spensertruex.com>
2019-12-06 15:34:27 -0800

    Remove details specific to my computer.

--- finalize.lisp
+++ finalize.lisp
@@ -5,7 +5,5 @@
 ;; start the polling timer process
 (start-clipboard-manager)
 
-(run-shell-command "feh --bg-fill ~/Pictures/calif-soviet.png")
-(run-shell-command "xgamma -gamma .8")
 (toggle-mode-line (current-screen)
                   (current-head))
